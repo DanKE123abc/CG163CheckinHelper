2270c4bc9989ea1907e6f949dfbe46eecd328aeb
DanKe <danke1024@foxmail.com>
2022-11-02 11:50:00 +0800

    提交

---
+++ index.py
@@ -0,0 +1,16 @@
+import time
+import json
+import requests
+import wechatpush
+
+#时间：2022/11/2
+#作者：蛋壳
+#Another: DanKe
+#备注：网易云游戏自动签到
+
+
+def handler(event, context):
+    print("Hello World!")
+
+if __name__ == '__main__':
+    handler(None, None)
