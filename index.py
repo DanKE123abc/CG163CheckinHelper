6c37ec6137abcdd5d51d5b538f97263838e46bb6
DanKe <danke1024@foxmail.com>
2023-01-09 19:11:57 +0800

    修复了一些bug

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import setting
 import requests
 import wechatpush
 
-#时间：2022/11/4
+#时间：2023/1/9
 #作者：蛋壳
 #Another: DanKe
 #备注：网易云游戏自动签到
@@ -13,8 +13,8 @@ sign_url = setting.Sign_url
 current = setting.Current
 headers = setting.headers
 
-def buildHearders(authorization):#更改headers
-        headers["Authorization"] = authorization
+def buildHeaders(authorization):#更改headers
+    headers["Authorization"] = authorization
 
 def sign():#签到
     result = requests.post(url=sign_url, headers=headers)
@@ -33,10 +33,11 @@ def writeMsg():#编辑信息
             signResult = "成功"
         elif signReturn.status_code == 400:
             signResult = "你已经签到过了"
-        elif signReturn.status_code == 401:
-            signResult = "登录信息可能已经失效"
         else:
             signResult = "失败，code="+str(signReturn.status_code)+"，请通过code判断失败原因"
+    elif checkReturn.status_code == 401:
+        checkResult = "登录信息可能已经失效"
+        signResult = "账号验证失败，无法签到"
     else:
         checkResult = "失败，code="+str(checkReturn.status_code)+"，请通过code判断失败原因"
         signResult = "账号验证失败，无法签到"
@@ -63,7 +64,7 @@ def handler(event, context):#这里是阿里云的入口，腾讯云要改成mai
         authorization = user['Authorization']
         pushid = user['pushid']
         try:
-            buildHearders(authorization)
+            buildHeaders(authorization)
             msg = writeMsg()
         except:
             msg = '签到失败，Authorization可能发生错误'
